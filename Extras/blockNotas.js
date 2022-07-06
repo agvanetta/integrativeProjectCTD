2244bed3add84ecec43b20fc37946bed13fe188e
Agustin Vanetta <agvanetta@gmail.com>
2022-07-06 00:47:15 -0300

    add_util

--- Extras/blockNotas.js
+++ Extras/blockNotas.js
@@ -1,6 +1,6 @@
 // birthday 2 .
 function birthdayCakeCandles(candles) {
-    var maxHeight = Math.max(...candles);
+    var maxHeight = Math.max(...candles);  /// let array2 = candles.sort(); let condition = array2[array2.length - 1];
     var maxHeightCount = 0;  
     for(var i = 0; i < candles.length; i++){
         if (candles[i] == maxHeight){
