47df1d8267fbd76e7d68e91d94624e1e85199bd7
Agustin Vanetta <agvanetta@gmail.com>
2022-07-10 20:01:35 -0300

    sout

--- Extras/practicajava/src/com/company/stdinStoutII.java
+++ Extras/practicajava/src/com/company/stdinStoutII.java
@@ -1,7 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class stdinStoutII {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 }
