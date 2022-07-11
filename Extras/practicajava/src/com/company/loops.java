b56120f0a37d0c82382e37da353f79e94c5fb5ec
Agustin Vanetta <agvanetta@gmail.com>
2022-07-11 00:13:50 -0300

    loop

--- Extras/practicajava/src/com/company/loops.java
+++ Extras/practicajava/src/com/company/loops.java
@@ -1,4 +1,19 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class loops {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(N+" x "+i+" = "+(N*i));
+        }
+
+        bufferedReader.close();
+    }
 }
