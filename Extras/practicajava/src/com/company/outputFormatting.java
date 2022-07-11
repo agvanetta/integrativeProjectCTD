59b022006d2e75cc5833c0323e5e9b978a34525b
Agustin Vanetta <agvanetta@gmail.com>
2022-07-10 23:54:17 -0300

    loop

--- Extras/practicajava/src/com/company/outputFormatting.java
+++ Extras/practicajava/src/com/company/outputFormatting.java
@@ -10,6 +10,23 @@ public class outputFormatting {
             String s1=sc.next();
             int x=sc.nextInt();
             //Complete this line
+            int spaces = (15-s1.length());
+            String add = "";
+            for (int j = 0; j < spaces; j++) {
+                add+=" ";
+            }
+            String x2 = "";
+            if (x < 100 && x >9){
+                x2 = "0"+x;
+                System.out.println(s1+add+x2);
+
+            } else if (x < 9){
+                x2 = "000";
+                System.out.println(s1+add+x2);
+            }
+            else {
+                System.out.println(s1+add+x);
+            }
         }
         System.out.println("================================");
 
